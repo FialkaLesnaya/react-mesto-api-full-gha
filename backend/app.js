c229dcd8c19ce6b74ddcb68a3ff0d2411ede58b0
Fialka Lesnaya <listik.vs@gmail.com>
2023-06-18 23:19:07 +0300

    Ошибка

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ const {
 const app = express();
 
 // app.use(cors({ origin: 'https://listik-fialki.nomoredomains.rocks' }));
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
